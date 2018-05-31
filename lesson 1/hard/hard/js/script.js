2c357797060fb459ffa516fd58a7462b5ad95309
Timur <dotsaev_timur@bk.ru>
2018-05-31 11:44:07 +0400

    домашка

--- lesson 1/hard/hard/js/script.js
+++ lesson 1/hard/hard/js/script.js
@@ -1,9 +1,6 @@
 let num =  33721;
 
-let num2 = eval(num.toString().split('').join('*'));
-
-
-let num3 = Math.pow(num2, 3),
-	split = eval(num3.toString().split('', 2));
-	alert(split);
+let num2 =  (Math.pow((eval(num.toString().split('').join('*'))), 3)).toString().split('', 2)
+	
+	alert(num2);
 	
